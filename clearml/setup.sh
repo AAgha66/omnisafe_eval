defd127675ecde14497260042bfb388797b3a5fd
AAgha66 <ahmed.agha@argmax.ai>
2024-06-03 03:37:11 +0200

    updating setup.sh

--- clearml/setup.sh
+++ clearml/setup.sh
@@ -7,11 +7,11 @@ cd $CLEARML_GIT_ROOT
 # now we need to tell clearml to use the python from our poetry env
 # this is in the general case (we use the system python above, so we could
 # have just hardcoded this as well)
-export venv_python_path="home/omnisafe/venv/bin/python"
-echo "Detected poetry python: $poetry_python_path"
+export venv_python_path="root/venv/bin/python"
+echo "Detected poetry python: $venv_python_path"
 cat > $CLEARML_CUSTOM_BUILD_OUTPUT << EOL
 {
-  "binary": "$poetry_python_path",
+  "binary": "$venv_python_path",
   "entry_point": "$CLEARML_GIT_ROOT/$CLEARML_TASK_SCRIPT_ENTRY",
   "working_dir": "$CLEARML_GIT_ROOT/$CLEARML_TASK_WORKING_DIR"
 }
